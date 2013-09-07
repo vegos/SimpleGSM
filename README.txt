77306a5e9d8cc3a05c01885a3813f0292ce26f05
Antonis <maglaras@gmail.com>
2013-09-07 17:43:15 +0300

    first commit

---
+++ README.txt
@@ -0,0 +1,39 @@
+SimpleGSM
+
+I bought myself a GSM Shield and wanted to use with an Arduino Leonardo and
+a LCD shield.
+
+The libraries I found where or incompatible with my GSM shield or with
+Leonardo, so I write a simple program for use with it.
+
+I slightly modified the GSM Shield for soft-power-on.
+
+GSM Shield:
+http://www.geeetech.com/gprsgsm-sim900-shield-board-arduino-compatible-p-610.html
+
+What I did is a jumper wire from pin 2 to JP (the left one, close to the
+antenna).
+Now, I can soft-power-on the shield using pin D2 instead of 9 (as D9 is used
+by LCD shield).
+
+The "el'cheapo" LCD/Keypad Shield I bought from ebay is using the D4, D5,
+D6, D7, D8, D9 pins for the LCD and A0 for the buttons (they are all
+connected to the same analog pin via resistors -- nice idea!).
+
+Arduino Leonardo has hardware support for TTL serial in pins D0, D1 (as for
+uploading sketches is using USB CDC communications).
+
+So you don't have to use SoftwareSerial library!
+
+
+
+The SimpleGSM can display all the serial communications in LCD (it's
+buffered for 50 lines/16 chars) and you can scroll with the UP/DOWN buttons.
+
+SELECT button diaplays a menu to (voice) dial a number.
+LEFT button answers a call.
+RIGHT button hangups/terminates a call.
+UP/DOWN buttons for scrolling the buffer.
+
+
+To be continued...
---
+++ SimpleGSM.ino
@@ -0,0 +1,354 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+unsigned char buffer[64]; // buffer array for data recieve over serial port
+int count=0;     // counter for buffer array 
+
+#define PWRPIN   2
+
+int lcd_key     = 0;
+int adc_key_in  = 0;
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+
+volatile int Col=0,Row=0;
+
+
+volatile char LCDBuffer[16][50][1]; 
+ 
+int dispRow;
+
+
+
+void setup()  
+{
+  for (int a=0; a<16; a++)
+    for (int b=0; b<50; b++)
+      LCDBuffer[a][b][1]=32;
+  lcd.begin(16, 2);              // start the library
+  lcd.clear();
+  lcd.setCursor(0,0);
+  //         0123456789012345
+  lcd.print("    GSM/GPRS    ");
+  lcd.setCursor(0,1);
+  lcd.print("Antonis Maglaras");
+  delay(5000);
+  pinMode(PWRPIN, OUTPUT);
+  // Open serial communications and wait for port to open:
+  Serial.begin(19200);
+  Serial.println("Power On");
+  lcd.clear();
+  lcd.print("Power On");
+  delay(1000);
+  Serial1.begin(19200);
+  PowerUp();
+  dispRow=0;
+}
+
+void loop() // run over and over
+{
+ if (Serial1.available())              // if date is comming from softwareserial port ==> data is comming from Serial1 shield
+ {
+   while(Serial1.available())          // reading data into char array 
+   {
+     buffer[count++]=Serial1.read();     // writing data into array
+     if(count == 64)break;
+   }
+   Serial.write(buffer,count);            // if no data transmission ends, write buffer to hardware serial port
+   clearBufferArray();              // call clearBufferArray function to clear the storaged data from the array
+   count = 0;                       // set counter of while loop to zero
+ }
+ if (Serial.available())            // if data is available on hardwareserial port ==> data is comming from PC or notebook
+   Serial1.write(Serial.read());       // write it to the Serial1 shield
+   
+   
+ if (read_LCD_buttons()==btnUP)
+ {
+   dispRow-=1;
+   if (dispRow<0)
+     dispRow=0;
+   DisplayTextOnLCD();
+   delay(150);
+ }
+ 
+ if (read_LCD_buttons()==btnDOWN)
+ {
+   dispRow+=1;
+   if (dispRow>Row)
+     dispRow=Row;
+   DisplayTextOnLCD();
+   delay(150);
+ }   
+ 
+ if (read_LCD_buttons()==btnLEFT)
+ {
+   delay(100);
+   AnswerCall();
+ }
+ 
+ if (read_LCD_buttons()==btnSELECT)
+ {
+   if (read_LCD_buttons()==btnSELECT)
+     while (read_LCD_buttons()==btnSELECT) {};
+   delay(100);
+   DialOut();
+ }
+ 
+ if (read_LCD_buttons()==btnRIGHT)
+ {
+   delay(100);
+   HangUpCall();
+ }
+ 
+}
+
+
+
+void clearBufferArray()              // function to clear buffer array
+{
+  for (int i=0; i<count; i++)
+  { 
+    if ((buffer[i]>=32) && (buffer[i]<127))
+    {
+      if (Col==16)
+      {
+        Row++;
+        if (Row>50)
+          Row=1;
+        Col=0;
+      }
+      LCDBuffer[Col][Row][1]=buffer[i];
+      Col++;
+    }
+    else
+      if (buffer[i]==13)
+      {
+        boolean twocrs=false;
+        for (int ena=0; ena<16; ena++)
+          if (LCDBuffer[ena][Row][1]!=32)
+             twocrs=true;
+        if (twocrs)
+        {
+          Row++;
+          Col=0;
+        }
+      }
+    buffer[i]=NULL;
+  }
+  dispRow=Row-1;
+  DisplayTextOnLCD();
+}
+
+void PowerUp()
+{
+ digitalWrite(PWRPIN,LOW);
+ delay(1000);
+ digitalWrite(PWRPIN,HIGH);
+ delay(2000);
+ digitalWrite(PWRPIN,LOW);
+}
+
+void PowerDown()
+{
+ digitalWrite(PWRPIN,LOW);
+ delay(1000);
+ digitalWrite(PWRPIN,HIGH);
+ delay(2000);
+ digitalWrite(PWRPIN,LOW);
+}
+
+
+// read the buttons
+int read_LCD_buttons()
+{
+ adc_key_in = analogRead(0);      // read the value from the sensor
+ // my buttons when read are centered at these valies: 0, 131, 306, 478, 720
+ // we add approx 50 to those values and check to see if we are close
+ if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+ if (adc_key_in < 50)   return btnRIGHT; 
+ if (adc_key_in < 185)  return btnUP;
+ if (adc_key_in < 360)  return btnDOWN;
+ if (adc_key_in < 530)  return btnLEFT;
+ if (adc_key_in < 770)  return btnSELECT;  
+ return btnNONE;  // when all others fail, return this...
+}
+ 
+
+void DisplayTextOnLCD()
+{
+  lcd.setCursor(0,0);
+  for (int tmp=0; (tmp<2); tmp++)
+    for (int tCol=0; tCol<16; tCol++)
+    {
+      lcd.setCursor(tCol,tmp);
+      lcd.print(LCDBuffer[tCol][tmp+dispRow-1][1]);
+    }
+}
+
+
+void AnswerCall()
+{
+  Serial1.println("ATA");
+  DisplayTextOnLCD();
+}
+
+void HangUpCall()
+{
+  Serial1.println("+++");
+  delay(50);
+  Serial1.println("ATH");
+  DisplayTextOnLCD();
+}
+
+void DialOut()
+{
+  lcd.clear();
+  lcd.print("Dial:");
+  boolean StayInside=true;
+  char* number = "";
+  int num=0;
+  int digit=1;
+  String dialno="";
+  boolean dialout=false;
+  while (StayInside)
+  {
+    lcd.setCursor(5+digit,0);
+    lcd.print(num);
+    switch (read_LCD_buttons())
+    {
+      case btnUP:
+        num+=1;
+        if (num>9)
+          num=0;
+        lcd.setCursor(5+digit,0);
+        lcd.print(num);
+        delay(200);
+        break;
+      case btnDOWN:
+        num-=1;
+        if (num<0)
+          num=9;
+        lcd.setCursor(5+digit,0);
+        lcd.print(num);         
+        delay(200);
+        break;
+      case btnLEFT:
+        lcd.setCursor(5+digit,0);
+        lcd.print(" ");
+        digit-=1;
+        if (digit<1)
+          digit=1;
+        num=0;
+        lcd.setCursor(5+digit,0);
+        lcd.print(num);         
+        dialno.substring(0, digit);
+        delay(200);
+        break;
+      case btnRIGHT:
+        digit+=1;
+        if (digit>10)
+        {
+          digit=10;
+          dialno.substring(0,9);
+          dialno+=String(num);
+        }
+        else
+        {
+          dialno+=String(num);
+          num=0;
+        }
+        lcd.setCursor(5+digit,0);
+        lcd.print(num);
+        delay(200);
+        break; 
+      case btnSELECT:
+        if (dialno.length()<10)
+          dialno+=String(num);
+        StayInside=false;
+        dialout=true;
+        delay(200);
+        break;
+    }
+  }
+  if (dialout)
+  {
+    lcd.clear();
+    lcd.print("Dial ");
+    lcd.print(dialno);
+    if (Selection())
+    {
+      Serial1.println("ATD ++30"+dialno);
+    }
+  }
+  DisplayTextOnLCD();
+}
+
+
+boolean Selection()
+{
+  lcd.setCursor(0,1);
+  //         0123456789012345
+  lcd.print("                ");
+  lcd.setCursor(0,1);
+  lcd.print("    YES   NO    ");
+  boolean StayInside=true;
+  boolean yes=true, whattoreturn=false;
+  while (StayInside)
+  {
+    if (yes)
+    {
+      lcd.setCursor(3,1);
+      lcd.print("[");
+      lcd.setCursor(7,1);
+      lcd.print("]");
+      lcd.setCursor(9,1);
+      lcd.print(" ");
+      lcd.setCursor(12,1);
+      lcd.print(" ");
+    }
+    else
+    {
+      lcd.setCursor(3,1);
+      lcd.print(" ");
+      lcd.setCursor(7,1);
+      lcd.print(" ");
+      lcd.setCursor(9,1);
+      lcd.print("[");
+      lcd.setCursor(12,1);
+      lcd.print("]");
+    }
+    if (read_LCD_buttons()==btnSELECT)
+    {
+      while (read_LCD_buttons()==btnSELECT);
+      if (yes) 
+      {
+        StayInside=false;
+        whattoreturn=true;
+      }
+      else
+      {
+        StayInside=false;
+        whattoreturn=false;
+      }
+    }
+    if (read_LCD_buttons()==btnLEFT)
+    {
+      yes=true;
+      delay(100);
+    }
+    if (read_LCD_buttons()==btnRIGHT)
+    {
+      yes=false;
+      delay(100);
+    }
+  }
+  if (yes)
+    return true;
+  else
+    return false;
+}
